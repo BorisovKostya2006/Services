ed15f8cfb0afde69e6f98620418f72fe78532cd5
Kostya Borisov <borisov.kostya999@gmail.com>
2025-05-20 17:47:51 +0300

    show notify

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.NotificationCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
         lateinit var binding: ActivityMainBinding
 
+
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,5 +23,26 @@ class MainActivity : AppCompatActivity() {
                 startService(MyServices.newIntent(this))
         }
 
+
+        binding.buttonShowNotify.setOnClickListener {
+                showNotify()
+        }
     }
+        @RequiresApi(Build.VERSION_CODES.O)
+        private fun showNotify(){
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                val notificationChannel = NotificationChannel("channel_id",
+                        "channel_name",
+                        NotificationManager.IMPORTANCE_DEFAULT)
+                notificationManager.createNotificationChannel(notificationChannel)
+                val notification = NotificationCompat.Builder(this, "channel_id")
+                        .setContentTitle("Title")
+                        .setContentText("Text")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .build()
+                notificationManager.notify(1,notification)
+        }
+
+
 }
+
