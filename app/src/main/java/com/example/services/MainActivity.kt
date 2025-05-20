db0647b30b5dee5515de7170bf510625694575c4
Kostya Borisov <borisov.kostya999@gmail.com>
2025-05-20 23:52:11 +0300

    Job Service, enqueue

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.services
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobService
+import android.app.job.JobWorkItem
+import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
@@ -11,28 +16,40 @@ import androidx.core.content.ContextCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-        lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    private var page = 0
 
 
-
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.buttonServices.setOnClickListener {
-                startService(MyServices.newIntent(this))
+            startService(MyServices.newIntent(this))
         }
 
 
-        binding.buttonShowNotify.setOnClickListener {
-                ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
+        binding.buttonJobService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
-                binding.buttonIntentService.setOnClickListener {
-                        ContextCompat.startForegroundService(this,MyIntentService.newIntent(this))
-                }
+        binding.buttonIntentService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyIntentService.newIntent(this))
+        }
+        binding.buttonJobService.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(1, componentName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setRequiresCharging(true)
+                .build()
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            val intent = MyJobService.newIntent(page++)
+            jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+
+        }
+
     }
-
 
 
 }
---
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -0,0 +1,61 @@
+package com.example.services
+
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.security.Provider
+
+class MyJobService : JobService() {
+    private val scope = CoroutineScope(Dispatchers.Main)
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("MyJobService","onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+        Log.d("MyJobService","onDestroy")
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d("MyJobService","onStartCommand")
+        scope.launch {
+            var workItem = params?.dequeueWork()
+            while (workItem != null){
+                val page = workItem.intent.getIntExtra("intExtra",0)
+                for (i in 0 until  5){
+                    delay(1000)
+                    Log.d("MyJobService","Timer $i, $page")
+                }
+                params?.completeWork(workItem)
+                workItem = params?.dequeueWork()
+            }
+            jobFinished(params,false)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d("MyJobService","onStopJob")
+        return false
+    }
+    companion object{
+        fun newIntent(page : Int) : Intent{
+            return Intent().putExtra("intExtra",page)
+        }
+    }
+}
